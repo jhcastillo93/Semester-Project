074ca42b997bde24f9b7288d6f70ffb18caadc02
Jason Castillo <jhcastillo@valdosta.edu>
2016-01-27 15:09:12 -0500

    Added carrierwave gem and successfully configured picture uploader and modified neccessaary files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
+  mount_uploader :image_url, PictureUploader
+  
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 # 
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-  }
+  
 end
