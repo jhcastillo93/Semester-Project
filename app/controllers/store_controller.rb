0a7ae7dfb2f56a9e26b4054a529ca95bde455519
Jason Castillo <jhcastillo@valdosta.edu>
2016-01-31 21:03:53 -0500

    Added session counter and added logic to display message only after 5 vists

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,11 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@time = Time.now.strftime("%I:%M %p %Z")
+  	@count = increment_count
+  	@shown_message = "You've been here #{@count} times" if @count >5
+  end
+  def increment_count
+  	session[:counter] ||= 0
+  	session[:counter] += 1
   end
 end
