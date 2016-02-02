9aeb084712b80ee01f26d8f47102fd5c454dae64
Jason Castillo <jhcastillo@valdosta.edu>
2016-02-02 17:26:59 -0500

    Began working on popularity feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129203740) do
+ActiveRecord::Schema.define(version: 20160202222232) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20160129203740) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.decimal  "popularity",  default: 0.0
   end
 
 end
