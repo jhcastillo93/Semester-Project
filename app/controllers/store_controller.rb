3d12a2f046c843ab748ed7dd1df2df60890e8e7e
Jason Castillo <jhcastillo@valdosta.edu>
2016-02-01 20:37:17 -0500

    Began working on the addtional requirements of project 3

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,7 @@ class StoreController < ApplicationController
   	@products = Product.order(:title)
   	@time = Time.now.strftime("%I:%M %p %Z")
   	@count = increment_count
-  	@shown_message = "You've been here #{@count} times" if @count >5
+  	@shown_message = ("You've been here #{@count} times without adding any books to your cart.")
   end
   def increment_count
   	session[:counter] ||= 0
