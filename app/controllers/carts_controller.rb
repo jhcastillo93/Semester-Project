42e1befcd0722787027bfa0871dd9547c643faba
Jason Castillo <jhcastillo@valdosta.edu>
2016-02-16 15:12:28 -0500

    Added AJAX to Popularity and fixed all issues with popularity updates

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,6 +58,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @prods = []
+    @cart.line_items.each do |item|
+      @prods << item
+    end
+    @cart.update_popularity
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,6 +17,9 @@ class Cart < ActiveRecord::Base
 
   	def decrease(line_item_id)
     	current_item = line_items.find(line_item_id)
+    	product = Product.find(current_item.product_id)
+    	product.popularity = product.popularity - 1
+        product.update_attributes(:popularity => product.popularity)
     	if current_item.quantity > 1
       		current_item.quantity -= 1
     	else
@@ -24,4 +27,15 @@ class Cart < ActiveRecord::Base
     	end
     	current_item
   	end
+
+  	def update_popularity
+  		line_items.each { |cart_items| 
+  			current_item = line_items.find(cart_items.id)
+  			decreaseBy = current_item.quantity
+  			product = Product.find(current_item.product_id)
+  			product.popularity = product.popularity - decreaseBy
+  			product.update_attributes(:popularity => product.popularity)
+		}
+	end
+
 end
